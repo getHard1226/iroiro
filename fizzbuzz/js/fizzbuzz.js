6ca8f397a15440591707b2ebdb6d74cb417602c6
getHard1226 <hi5.nakash1ma@gmail.com>
2018-10-30 21:02:30 +0900

    %以外でfizzbuzz

--- fizzbuzz/js/fizzbuzz.js
+++ fizzbuzz/js/fizzbuzz.js
@@ -1,7 +1,10 @@
 var count = 0;
+var count1 = 0;
 var FIZZ = 'Fizz!';
 var BUZZ = 'Buzz!';
 var FIZZ_BUZZ = 'Fizz Buzz!';
+var fizzFlg = false;
+var buzzFlg = false;
 
 window.addEventListener('load', function() {
 	init();
@@ -9,6 +12,9 @@ window.addEventListener('load', function() {
 
 function init() {
 	count = 0;
+	count1 = 0;
+	fizzFlg = false;
+	buzzFlg = false;
 }
 
 function nextClick() {
@@ -27,5 +33,46 @@ function nextClick() {
 			document.getElementById('fizzbuzzArea').innerHTML
 				= count;
 		}
+	}
+}
+
+function nextClick2() {
+	++count1;
+	fizzJudge();
+	buzzJudge();
+	show();
+}
+
+function fizzJudge() {
+	var t = count1/3;
+	if ((t-Math.floor(t)) == 0) {
+		fizzFlg = true;
+	} else {
+		fizzFlg = false;
+	}
+}
+
+function buzzJudge() {
+	var t = count1/5;
+	if ((t-Math.floor(t)) == 0) {
+		buzzFlg = true;
+	} else {
+		buzzFlg = false;
+	}
+}
+
+function show() {
+	if (fizzFlg && buzzFlg) {
+		document.getElementById('fizzbuzz2').innerHTML
+			= count1 + ' ' + FIZZ_BUZZ;
+	} else if (fizzFlg) {
+		document.getElementById('fizzbuzz2').innerHTML
+			= count1 + ' ' + FIZZ;
+	} else if (buzzFlg) {
+		document.getElementById('fizzbuzz2').innerHTML
+			= count1 + ' ' + BUZZ;
+	} else {
+		document.getElementById('fizzbuzz2').innerHTML
+			= count1;
 	}
 }
